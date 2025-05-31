b6ea71b8dae9121232f107213793671df62e7a59
Nicholas Chiang <nicholas.chiang@numbersstation.ai>
2025-05-31 05:20:25 -0600

    fix: use `min-h-dvh` for login page
    Mobile devices have dynamic viewports (iOS, Android).
    
    Ref: https://stackoverflow.com/questions/61308575/tailwind-h-screen-doesn-t-work-properly-on-mobile-devices
    Ref: https://tailwindcss.com/docs/height#matching-dynamic-viewport

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
   import { Button } from "$lib/components/ui/button"
   import { Google } from "$lib/components/icons/google"
-  import { ShinyText } from '$lib/components/ui/shiny-text'
+  import { ShinyText } from "$lib/components/ui/shiny-text"
 
   let { data } = $props()
 </script>
 
 <main
-  class="mx-auto flex h-screen w-screen max-w-xl flex-col items-center justify-center space-y-12 p-4"
+  class="mx-auto flex min-h-dvh max-w-xl flex-col items-center justify-center space-y-12 p-4"
 >
   <header class="flex flex-col items-center">
     <ShinyText class="text-8xl font-black">G</ShinyText>
