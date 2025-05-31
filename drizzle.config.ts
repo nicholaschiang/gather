b8c4e9ab5362b152cc6d1b5b6b6043a18f03c5d4
Nicholas Chiang <nicholas.chiang@numbersstation.ai>
2025-05-31 05:09:22 -0600

    fix: support migrating turso database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,28 @@
-import { defineConfig } from "drizzle-kit"
+import { defineConfig, type Config } from "drizzle-kit"
 
 if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set")
 
-export default defineConfig({
-  schema: "./src/lib/server/db/schema.ts",
-  dbCredentials: { url: process.env.DATABASE_URL },
-  verbose: true,
-  strict: true,
-  dialect: "sqlite",
-})
+let config: Config
+
+if (process.env.DATABASE_AUTH_TOKEN) {
+  config = defineConfig({
+    schema: "./src/lib/server/db/schema.ts",
+    dbCredentials: {
+      url: process.env.DATABASE_URL,
+      authToken: process.env.DATABASE_AUTH_TOKEN,
+    },
+    verbose: true,
+    strict: true,
+    dialect: "turso",
+  })
+} else {
+  config = defineConfig({
+    schema: "./src/lib/server/db/schema.ts",
+    dbCredentials: { url: process.env.DATABASE_URL },
+    verbose: true,
+    strict: true,
+    dialect: "sqlite",
+  })
+}
+
+export default config
