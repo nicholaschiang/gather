8073c536f408d7fecd3320c9a8237a8c88d1ea51
Nicholas Chiang <nicholas.chiang@numbersstation.ai>
2025-06-01 00:43:13 -0600

    feat: add gathering time suggestions
    Gathering creators will see meeting time suggestions when they open a
    gathering. These suggestions are the soonest 30m time slots when all
    the gathering participants are available.
    
    The suggestion algorithm is likely very non-optimal. Further work is
    likely required to make this faster and more scalable.
    
    A later patch will let the gathering creator configure the gathering
    duration (i.e. so it is no longer hard coded to 30m).

--- src/routes/g/[gatheringId]/+page.server.ts
+++ src/routes/g/[gatheringId]/+page.server.ts
@@ -1,9 +1,101 @@
 import { db } from "$lib/server/db"
+import { google } from "googleapis"
 import { redirectToGoogle } from "$lib/server/google"
 import * as table from "$lib/server/db/schema"
 import { eq, and } from "drizzle-orm"
-import { redirect, error } from "@sveltejs/kit"
+import { redirect, error, fail } from "@sveltejs/kit"
 import { alias } from "drizzle-orm/sqlite-core"
+import { env } from "$env/dynamic/private"
+import { z } from "zod"
+
+// Desired duration of the meeting in ms.
+// TODO This should be configured in the gathering itself.
+const DURATION_IN_MS = 1000 * 60 * 30
+const DAY_IN_MS = 1000 * 60 * 60 * 24
+const LOOKAHEAD = 7
+
+export type TimePeriod = { start: Date; end: Date }
+
+// Get the available meeting times for a gathering.
+// Fetches availability from Google Calendar and finds overlaps.
+async function getPossibleMeetingTimes(participants: table.User[]) {
+  console.time("getPossibleMeetingTimes")
+
+  // Start the search now, rounded up to the nearest 10 minutes.
+  // Ex: If it is 4:32pm now, we look for times starting at 4:40pm.
+  const now = new Date()
+  const timeMin = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate(),
+    now.getHours(),
+    Math.ceil(now.getMinutes() / 10) * 10,
+  )
+  const timeMax = new Date(timeMin.valueOf() + LOOKAHEAD * DAY_IN_MS)
+
+  // Get all of the busy time periods for every participant.
+  const busyTimePeriods: TimePeriod[] = []
+  console.time("api")
+  for (const participant of participants) {
+    // TODO Perhaps we should store the access_token and other values to make this
+    // more efficient? Right now, we refresh the access token every time.
+    const tokens = { refresh_token: participant.googleRefreshToken }
+    const authClient = new google.auth.OAuth2(
+      env.GOOGLE_CLIENT_ID,
+      env.GOOGLE_CLIENT_SECRET,
+      env.GOOGLE_REDIRECT_URI,
+    )
+    authClient.setCredentials(tokens)
+    const calendar = google.calendar({ version: "v3", auth: authClient })
+    // TODO Get all of the visible/active calendars and get free/busy from all
+    // of them. Perhaps let the user configure which calendars to check?
+    const res = await calendar.freebusy.query({
+      requestBody: {
+        timeMin: timeMin.toISOString(),
+        timeMax: timeMax.toISOString(),
+        items: [{ id: "primary" }],
+      },
+    })
+    const busy = res.data.calendars?.primary.busy
+    console.log(`busy times for ${participant.email}:`, busy)
+    busy?.forEach((busyTime) => {
+      if (busyTime.start == null) throw new Error("Busy start is null.")
+      if (busyTime.end == null) throw new Error("Busy end is null.")
+      busyTimePeriods.push({
+        start: new Date(busyTime.start),
+        end: new Date(busyTime.end),
+      })
+    })
+  }
+  console.timeEnd("api")
+  console.log("busy time periods:", busyTimePeriods)
+
+  // Sort the busy time periods by start time, ascending (i.e. earlier first).
+  //busyTimePeriods.sort((a, b) => a.start.valueOf() - b.start.valueOf())
+
+  function overlaps(a: TimePeriod, b: TimePeriod) {
+    return (
+      a.start.valueOf() < b.end.valueOf() && b.start.valueOf() < a.end.valueOf()
+    )
+  }
+
+  // Start at the timeMin and search for available slots of DURATION minutes.
+  const freeTimePeriods: TimePeriod[] = []
+  let start: Date = timeMin
+  let end: Date
+  while (start.valueOf() < timeMax.valueOf()) {
+    end = new Date(start.valueOf() + DURATION_IN_MS)
+    if (busyTimePeriods.some((b) => overlaps({ start, end }, b))) {
+      console.warn("time period is busy:", { start, end })
+    } else {
+      freeTimePeriods.push({ start, end })
+    }
+    start = end
+  }
+  console.log("free time periods:", freeTimePeriods)
+  console.timeEnd("getPossibleMeetingTimes")
+  return freeTimePeriods
+}
 
 function isNotNull<T>(value: T | null): value is T {
   return value != null
@@ -23,11 +115,13 @@ export async function load(event) {
     .where(eq(table.gathering.id, event.params.gatheringId))
   const row = rows.at(0)
   if (!row) error(404)
+  const attendees = rows.map((row) => row.user).filter(isNotNull)
   return {
     user: event.locals.user,
-    attendees: rows.map((row) => row.user).filter(isNotNull),
     creator: row.creator,
     gathering: row.gathering,
+    attendees,
+    times: getPossibleMeetingTimes([row.creator, ...attendees]),
   }
 }
 
@@ -44,7 +138,14 @@ export const actions = {
     })
   },
   delete: async (event) => {
-    if (!event.locals.user) error(401)
+    if (!event.locals.user) return error(401)
+    const rows = await db
+      .select()
+      .from(table.gathering)
+      .where(eq(table.gathering.id, event.params.gatheringId))
+    const gathering = rows.at(0)
+    if (!gathering) return error(404)
+    if (gathering.creatorId !== event.locals.user.id) return error(403)
     await db
       .delete(table.gathering)
       .where(
@@ -53,10 +154,10 @@ export const actions = {
           eq(table.gathering.creatorId, event.locals.user.id),
         ),
       )
-    redirect(302, "/")
+    return redirect(302, "/")
   },
   leave: async (event) => {
-    if (!event.locals.user) error(401)
+    if (!event.locals.user) return error(401)
     await db
       .delete(table.relUserGathering)
       .where(
@@ -65,6 +166,33 @@ export const actions = {
           eq(table.relUserGathering.userId, event.locals.user.id),
         ),
       )
-    redirect(302, "/")
+    return redirect(302, "/")
+  },
+  time: async (event) => {
+    if (!event.locals.user) return error(401)
+    const rows = await db
+      .select()
+      .from(table.gathering)
+      .where(eq(table.gathering.id, event.params.gatheringId))
+    const gathering = rows.at(0)
+    if (!gathering) return error(404)
+    if (gathering.creatorId !== event.locals.user.id) return error(403)
+    const schema = z.object({
+      start: z.coerce.date(),
+      end: z.coerce.date(),
+    })
+    const data = Object.fromEntries(await event.request.formData())
+    const form = schema.safeParse(data)
+    if (!form.success) return fail(400, { form })
+    await db
+      .update(table.gathering)
+      .set({ start: form.data.start, end: form.data.end })
+      .where(
+        and(
+          eq(table.gathering.id, event.params.gatheringId),
+          eq(table.gathering.creatorId, event.locals.user.id),
+        ),
+      )
+    return { form }
   },
 }
--- src/routes/g/[gatheringId]/+page.svelte
+++ src/routes/g/[gatheringId]/+page.svelte
@@ -2,6 +2,7 @@
   import { Button, buttonVariants } from "$lib/components/ui/button"
   import { Google } from "$lib/components/icons/google"
   import {
+    ArrowRight,
     Clock,
     DoorOpen,
     MoreHorizontal,
@@ -17,9 +18,11 @@
   import * as Avatar from "$lib/components/ui/avatar"
   import * as DropdownMenu from "$lib/components/ui/dropdown-menu"
   import { type User as UserT } from "$lib/server/db/schema"
+  import { cn } from "$lib/utils"
+  import { type TimePeriod } from "./+page.server"
 
   let { data } = $props()
-  let { user } = data
+  let user = $derived(data.user)
   let participantCount = $derived(data.attendees.length + 1)
   let showDeleteButton = $derived(user?.id === data.gathering.creatorId)
   let showLeaveButton = $derived(data.attendees.some((a) => a.id === user?.id))
@@ -34,7 +37,7 @@
 </script>
 
 <header
-  class="bg-background sticky top-0 flex items-center justify-between px-4 pt-4 lg:mx-auto lg:max-w-xl"
+  class="bg-background/80 sticky top-0 z-10 flex items-center justify-between px-4 pt-4 pb-2 backdrop-blur lg:mx-auto lg:max-w-xl"
 >
   <h2 class="text-sm font-medium text-neutral-400 dark:text-neutral-600">
     Gathering
@@ -88,18 +91,100 @@
     >
   </div>
 </header>
-<main class="space-y-4 p-4 lg:mx-auto lg:max-w-xl">
+<main class="space-y-4 px-4 lg:mx-auto lg:max-w-xl">
   <h1 class="text-xl">{data.gathering.title}</h1>
 
   <hr class="-mr-4 lg:mr-0" />
   <div class="flex space-x-4">
-    <Clock class="mt-1 size-4 text-neutral-400 dark:text-neutral-600" />
-    <div>Time has not been set yet.</div>
+    <Clock
+      class="mt-1 size-4 shrink-0 text-neutral-400 dark:text-neutral-600"
+    />
+    <div class="flex flex-col gap-2">
+      {#snippet timeDisplay(time: TimePeriod)}
+        <div class="flex items-center gap-2">
+          <div class="w-24">
+            {time?.start.toLocaleString(undefined, {
+              hour: "numeric",
+              minute: "numeric",
+            })}
+          </div>
+          <ArrowRight
+            class="mr-2 size-4 text-neutral-400 dark:text-neutral-600"
+          />
+          <div>
+            {time?.end.toLocaleString(undefined, {
+              hour: "numeric",
+              minute: "numeric",
+            })}
+          </div>
+          <div class="text-neutral-400 dark:text-neutral-600">30m</div>
+        </div>
+        <div>
+          {time?.start.toLocaleString(undefined, {
+            weekday: "short",
+            day: "numeric",
+            month: "short",
+            year:
+              new Date().getFullYear() !== time.start.getFullYear()
+                ? "numeric"
+                : undefined,
+          })}
+        </div>
+      {/snippet}
+      {#snippet possibleTimeButton(time?: TimePeriod)}
+        <button
+          type={time ? "submit" : "button"}
+          class={cn(
+            buttonVariants({ variant: "outline" }),
+            "gradient-border block h-16 w-64 px-3 py-0 text-left text-sm",
+            time ? "done" : "cursor-wait",
+          )}
+        >
+          <div class="gradient-border-outline"></div>
+          {#if time}
+            {@render timeDisplay(time)}
+          {/if}
+        </button>
+      {/snippet}
+
+      {#if data.gathering.start && data.gathering.end}
+        {@render timeDisplay({
+          start: data.gathering.start,
+          end: data.gathering.end,
+        })}
+      {:else if data.gathering.creatorId === user?.id}
+        {#await data.times}
+          <p>Finding possible times...</p>
+          {#each { length: 3 }}
+            {@render possibleTimeButton()}
+          {/each}
+        {:then times}
+          <p>Possible times</p>
+          {#each times.slice(0, 3) as time}
+            <form method="POST" action="?/time">
+              <input
+                type="hidden"
+                name="start"
+                value={time.start.toISOString()}
+              />
+              <input type="hidden" name="end" value={time.end.toISOString()} />
+              {@render possibleTimeButton(time)}
+            </form>
+          {/each}
+        {:catch error}
+          <p class="text-destructive line-clamp-2">
+            Error getting times: {error.message}
+          </p>
+        {/await}
+      {:else}
+        <p>The creator hasn't confirmed a time yet.</p>
+      {/if}
+    </div>
   </div>
 
   <hr class="-mr-4 lg:mr-0" />
   <div class="flex space-x-4">
-    <User class="mt-1 size-4 text-neutral-400 dark:text-neutral-600" />
+    <User class="mt-1 size-4 shrink-0 text-neutral-400 dark:text-neutral-600" />
     <div class="space-y-4">
       <p>
         {participantCount} participant{#if participantCount !== 1}s{/if}
@@ -169,6 +254,16 @@
 </main>
 
 <style>
+  :root {
+    --gradient-color-1: 215, 98%, 61%;
+    --gradient-color-2: 256, 98%, 72%;
+    --gradient-color-3: 354, 98%, 61%;
+    --gradient-color-4: 133, 96%, 67%;
+    --gradient-color-5: 38, 60%, 74%;
+    --gradient-color-6: 222, 67%, 73%;
+    --gradient-color-7: 343, 68%, 79%;
+  }
+
   .gradient {
     position: relative;
 
@@ -176,31 +271,39 @@
       background-image:
         radial-gradient(
           at 27% 37%,
-          hsla(215, 98%, 61%, 1) 0px,
+          hsla(var(--gradient-color-1), 1) 0px,
           transparent 50%
         ),
         radial-gradient(
           at 97% 21%,
-          hsla(256, 98%, 72%, 1) 0px,
+          hsla(var(--gradient-color-2), 1) 0px,
           transparent 50%
         ),
         radial-gradient(
           at 52% 99%,
-          hsla(354, 98%, 61%, 1) 0px,
+          hsla(var(--gradient-color-3), 1) 0px,
           transparent 50%
         ),
         radial-gradient(
           at 10% 29%,
-          hsla(133, 96%, 67%, 1) 0px,
+          hsla(var(--gradient-color-4), 1) 0px,
           transparent 50%
         ),
-        radial-gradient(at 97% 96%, hsla(38, 60%, 74%, 1) 0px, transparent 50%),
+        radial-gradient(
+          at 97% 96%,
+          hsla(var(--gradient-color-5), 1) 0px,
+          transparent 50%
+        ),
         radial-gradient(
           at 33% 50%,
-          hsla(222, 67%, 73%, 1) 0px,
+          hsla(var(--gradient-color-6), 1) 0px,
           transparent 50%
         ),
-        radial-gradient(at 79% 53%, hsla(343, 68%, 79%, 1) 0px, transparent 50%);
+        radial-gradient(
+          at 79% 53%,
+          hsla(var(--gradient-color-7), 1) 0px,
+          transparent 50%
+        );
       position: absolute;
       content: "";
       width: 100%;
@@ -208,8 +311,122 @@
       filter: blur(20px) saturate(150%);
       z-index: -1;
       top: 0;
+      left: 0;
       opacity: 0.2;
       transform: translateZ(0);
+    }
+  }
+
+  .gradient-border {
+    --gradient-border-radius: var(--radius-md);
+    position: relative;
+    border-radius: var(--gradient-border-radius);
+
+    & > .gradient-border-outline {
+      position: absolute;
+      inset: -1px;
+      overflow: hidden;
+      border-radius: var(--gradient-border-radius);
+      z-index: -1;
+    }
+
+    & > .gradient-border-outline::before {
+      content: "";
+      position: absolute;
+      inset: -20px;
+      animation: gradient-border-spin 2.5s ease-in-out infinite;
+      background: conic-gradient(
+        hsla(var(--gradient-color-1), 0.8),
+        hsla(var(--gradient-color-2), 0.6),
+        hsla(var(--gradient-color-3), 0.4),
+        hsla(var(--gradient-color-5), 0.1),
+        hsla(var(--gradient-color-4), 0.4),
+        hsla(var(--gradient-color-6), 0.6),
+        hsla(var(--gradient-color-7), 0.8)
+      );
+      filter: blur(24px);
+      opacity: 0;
+    }
+
+    &.done > .gradient-border-outline::before {
+      background:
+        radial-gradient(
+          at 27% 37%,
+          hsla(var(--gradient-color-1), 1) 0px,
+          transparent 50%
+        ),
+        radial-gradient(
+          at 97% 21%,
+          hsla(var(--gradient-color-2), 1) 0px,
+          transparent 50%
+        ),
+        radial-gradient(
+          at 52% 99%,
+          hsla(var(--gradient-color-3), 1) 0px,
+          transparent 50%
+        ),
+        radial-gradient(
+          at 10% 29%,
+          hsla(var(--gradient-color-4), 1) 0px,
+          transparent 50%
+        ),
+        radial-gradient(
+          at 97% 96%,
+          hsla(var(--gradient-color-5), 1) 0px,
+          transparent 50%
+        ),
+        radial-gradient(
+          at 33% 50%,
+          hsla(var(--gradient-color-6), 1) 0px,
+          transparent 50%
+        ),
+        radial-gradient(
+          at 79% 53%,
+          hsla(var(--gradient-color-7), 1) 0px,
+          transparent 50%
+        );
+      animation-delay: 0.2s;
+      animation: gradient-border-fade 2.5s ease-out forwards;
+    }
+
+    & > .gradient-border-outline::after {
+      content: "";
+      position: absolute;
+      inset: 1.5px;
+      border-radius: calc(var(--gradient-border-radius) - 1.5px);
+      background: var(--background);
+    }
+  }
+
+  @keyframes gradient-border-spin {
+    0% {
+      transform: rotate(0deg);
+      opacity: 0;
+    }
+    15% {
+      opacity: 1;
+    }
+    85% {
+      opacity: 1;
+    }
+    to {
+      transform: rotate(1turn);
+      opacity: 0;
+    }
+  }
+
+  @keyframes gradient-border-fade {
+    0% {
+      opacity: 0;
+      filter: blur(24px);
+    }
+    50% {
+      opacity: 1;
+      filter: blur(24px);
+    }
+    to {
+      opacity: 0;
+      filter: blur(24px);
     }
   }
 </style>
