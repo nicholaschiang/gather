nicholaschiang/gather

commit ae739c73bc9ad36942c8a97dcd7c32570e9fc2e4

fix(server/google): get public keys every time

src/lib/server/google.ts
@@ -55,27 +55,23 @@ export async function redirectToGoogle(state: Omit<table.AuthState, "id">) {
   return redirect(302, authUrl)
 }
 
-// TODO: Get this from Google.
-// https://www.googleapis.com/oauth2/v3/certs
-const keys = {
-  keys: [
-    {
-      kty: "RSA",
-      n: "qGs032k_zvMoe1U7O9qC6k-oA1E3Ftz9z37EtjHBZ9sLYfDKPDsOhjS2HQCT69Ms6ET02wE2T9aw6gtjrhqiZPS_ZZhauy8OApqyMFGiJNuuyduUBRRvO_bo2_nE4wxCmDUDHh1wH-Wk04JctHtZA22F42f2n3nGQEQ-6wqSpbjiipwdXv8RQFmo0mObB3mnw1a81d-0kLrpnLeq-Bp0nFb5lJIan2sQOmuyC9r3q-9geM3E4JY6_0Beh4_zEM41c71f0qIfqznORQVpHbOmseH_zS75C91N8__sD7iOeO3zZEUO2LJ3OC_xj75BQf61NTXW_ia_-AoXhMnrGm-ZqQ",
-      use: "sig",
-      e: "AQAB",
-      alg: "RS256",
-      kid: "baa64efc13ef236be921f922e3a67cc99415db9b",
-    },
-    {
-      use: "sig",
-      alg: "RS256",
-      e: "AQAB",
-      kid: "bb43469594451820148b339c588aede305103919",
-      n: "s9s2WkplZRxGVqfBk8o2jvr664mU7bdBB-YesCzBjlHYJetgAt_kKEPEmHmTlget39m02MW_YqDmyu6foDw8xcjnIcXUplHG3klEBHvtctHbP6SYwBiG-5Y3fqyen8JtqstkWXOFfSqVVot7rVSCSwPqtw-zToCXi_47MEuIFALmSLuOzVkk4POHWi7jGrieBxHNcs_xHTKngd0xO4ZpjscYMkdGbBO9-Q_VBEEW3ZtE6Ac34H7f_EF45kGBFSBdyFJjvuYpyXrsNvxZK1pE_snDuStPS95rajzqDhAckTryStCu6uuL02rPifIS-8JFUSceZiSYsbRVGkb5xTbQIQ",
-      kty: "RSA",
-    },
-  ],
+const keySchema = z.object({
+  e: z.string(),
+  kty: z.string(),
+  n: z.string(),
+  use: z.string(),
+  alg: z.string(),
+  kid: z.string(),
+})
+const keysSchema = z.object({ keys: z.array(keySchema) })
+
+async function getGooglePublicKey(): Promise<jwt.PublicKey> {
+  // TODO: Cache the keys and only refetch if decoding fails.
+  const response = await fetch("https://www.googleapis.com/oauth2/v3/certs")
+  const data = keysSchema.parse(await response.json())
+  // TODO: Why do we use the first key from Google? Is it always?
+  const key = { key: data.keys[0], format: "jwk" as const }
+  return key
 }
 
 export async function handleGoogleCallback(event: RequestEvent) {
@@ -112,9 +108,7 @@ export async function handleGoogleCallback(event: RequestEvent) {
 
   if (!tokens.id_token) throw new Error("No ID token found.")
 
-  // TODO: Why do we use the first key from Google? Is it always?
-  const key = { key: keys.keys[0], format: "jwk" as const }
-  const decoded = jwt.verify(tokens.id_token, key)
+  const decoded = jwt.verify(tokens.id_token, await getGooglePublicKey())
   const idToken = idTokenSchema.parse(decoded)
   console.log("decoded", decoded)
   console.log("idToken", idToken)

commit e948f5044a44f40315cba825610f62808c8cd134

fix: get correct public key for JWT verification
Verify the JWT using the getKey callback with the `jsonwebtoken`
library. This can be used for any JWT, I believe. Use `node-jwks-rsa` as
a way to fetch the public keys used in verification.

I could probably also just use the Node.js `google-auth-library` but
this is kind of fun and just as easy.

Ref: https://github.com/googleapis/google-auth-library-nodejs
Ref: https://developers.google.com/identity/gsi/web/guides/verify-google-id-token#node.js
Ref: https://www.npmjs.com/package/jsonwebtoken#jwtverifytoken-secretorpublickey-options-callback
Ref: https://github.com/auth0/node-jwks-rsa

Fixes: ae739c7 ("fix(server/google): get public keys every time")

pnpm-lock.yaml
@@ -41,6 +41,9 @@ importers:
       jsonwebtoken:
         specifier: ^9.0.2
         version: 9.0.2
+      jwks-rsa:
+        specifier: ^3.2.0
+        version: 3.2.0
       uuid:
         specifier: ^11.1.0
         version: 11.1.0
@@ -1570,11 +1573,26 @@ packages:
   '@types/aria-query@5.0.4':
     resolution: {integrity: sha512-rfT93uj5s0PRL7EzccGMs3brplhcrghnDoV26NqKhCAS1hVo+WdNsPvE/yb6ilfr5hi2MEk6d5EWJTKdxg8jVw==}
 
+  '@types/body-parser@1.19.6':
+    resolution: {integrity: sha512-HLFeCYgz89uk22N5Qg3dvGvsv46B8GLvKKo1zKG4NybA8U2DiEO3w9lqGg29t/tfLRJpJ6iQxnVw4OnB7MoM9g==}
+
+  '@types/connect@3.4.38':
+    resolution: {integrity: sha512-K6uROf1LD88uDQqJCktA4yzL1YYAK6NgfsI0v/mTgyPKWsX1CnJ0XPSDhViejru1GcRkLWb8RlzFYJRqGUbaug==}
+
   '@types/cookie@0.6.0':
     resolution: {integrity: sha512-4Kh9a6B2bQciAhf7FSuMRRkUWecJgJu9nPnx3yzpsfXX/c50REIqpHY4C82bXP90qrLtXtkDxTZosYO3UpOwlA==}
 
   '@types/estree@1.0.7':
     resolution: {integrity: sha512-w28IoSUCJpidD/TGviZwwMJckNESJZXFu7NBZ5YJ4mEUnNraUn9Pm8HSZm/jDF1pDWYKspWE7oVphigUPRakIQ==}
+
+  '@types/express-serve-static-core@4.19.7':
+    resolution: {integrity: sha512-FvPtiIf1LfhzsaIXhv/PHan/2FeQBbtBDtfX2QfvPxdUelMDEckK08SM6nqo1MIZY3RUlfA+HV8+hFUSio78qg==}
+
+  '@types/express@4.17.25':
+    resolution: {integrity: sha512-dVd04UKsfpINUnK0yBoYHDF3xu7xVH4BuDotC/xGuycx4CgbP48X/KF/586bcObxT0HENHXEU8Nqtu6NR+eKhw==}
+
+  '@types/http-errors@2.0.5':
+    resolution: {integrity: sha512-r8Tayk8HJnX0FztbZN7oVqGccWgw98T/0neJphO91KkmOzug1KkofZURD4UaD5uH8AqcFLfdPErnBod0u71/qg==}
 
   '@types/json-schema@7.0.15':
     resolution: {integrity: sha512-5+fP8P8MFNC+AyZCDxrB2pkZFPGzqQWUzpSeuuVLvm8VMcorNYavBqoFcxK8bQz4Qsbn4oUEEem4wDLfcysGHA==}
@@ -1585,6 +1603,9 @@ packages:
   '@types/mdx@2.0.13':
     resolution: {integrity: sha512-+OWZQfAYyio6YkJb3HLxDrvnx6SWWDbC0zVPfBRzUk0/nqoDyf6dNxQi3eArPe8rJ473nobTMQ/8Zk+LxJ+Yuw==}
 
+  '@types/mime@1.3.5':
+    resolution: {integrity: sha512-/pyBZWSLD2n0dcHE3hq8s8ZvcETHtEuF+3E7XVt0Ig2nvsVQXdghHVcEkIWjy9A0wKfTn97a/PSDYohKIlnP/w==}
+
   '@types/ms@2.1.0':
     resolution: {integrity: sha512-GsCCIZDE/p3i96vtEqx+7dBUGXrc7zeSK3wwPHIaRThS+9OhWIXRqzs4d6k1SVU8g91DrNRWxWUGhp5KXQb2VA==}
 
@@ -1594,8 +1615,23 @@ packages:
   '@types/pug@2.0.10':
     resolution: {integrity: sha512-Sk/uYFOBAB7mb74XcpizmH0KOR2Pv3D2Hmrh1Dmy5BmK3MpdSa5kqZcg6EKBdklU0bFXX9gCfzvpnyUehrPIuA==}
 
+  '@types/qs@6.14.0':
+    resolution: {integrity: sha512-eOunJqu0K1923aExK6y8p6fsihYEn/BYuQ4g0CxAAgFc4b/ZLN4CrsRZ55srTdqoiLzU2B2evC+apEIxprEzkQ==}
+
+  '@types/range-parser@1.2.7':
+    resolution: {integrity: sha512-hKormJbkJqzQGhziax5PItDUTMAM9uE2XXQmM37dyd4hVM+5aVl7oVxMVUiVQn2oCQFN/LKCZdvSM0pFRqbSmQ==}
+
   '@types/react@19.1.4':
     resolution: {integrity: sha512-EB1yiiYdvySuIITtD5lhW4yPyJ31RkJkkDw794LaQYrxCSaQV/47y5o1FMC4zF9ZyjUjzJMZwbovEnT5yHTW6g==}
+
+  '@types/send@0.17.6':
+    resolution: {integrity: sha512-Uqt8rPBE8SY0RK8JB1EzVOIZ32uqy8HwdxCnoCOsYrvnswqmFZ/k+9Ikidlk/ImhsdvBsloHbAlewb2IEBV/Og==}
+
+  '@types/send@1.2.1':
+    resolution: {integrity: sha512-arsCikDvlU99zl1g69TcAB3mzZPpxgw0UQnaHeC1Nwb015xp8bknZv5rIfri9xTOcMuaVgvabfIRA7PSZVuZIQ==}
+
+  '@types/serve-static@1.15.10':
+    resolution: {integrity: sha512-tRs1dB+g8Itk72rlSI2ZrW6vZg0YrLI81iQSTkMmOqnqCaNr/8Ek4VwWcN5vZgCYWbg/JJSGBlUaYGAOP73qBw==}
 
   '@types/uuid@9.0.8':
     resolution: {integrity: sha512-jg+97EGIcY9AGHJJRaaPVgetKDsrTgbRjQ5Msgjh/DQKEFl0DtyRr/VCOyD1T2R1MNeWPK/u7JoGhlDZnKBAfA==}
@@ -2876,6 +2912,9 @@ packages:
   joi@17.13.3:
     resolution: {integrity: sha512-otDA4ldcIx+ZXsKHWmp0YizCweVRZG96J10b0FevjfuncLO1oX59THoAmHkNubYJ+9gWsYsp5k8v4ib6oDv1fA==}
 
+  jose@4.15.9:
+    resolution: {integrity: sha512-1vUQX+IdDMVPj4k8kOxgUqlcK518yluMuGZwqlr44FS1ppZB/5GWh4rZG89erpOBOJjU/OBsnCVFfapsRz6nEA==}
+
   js-base64@3.7.7:
     resolution: {integrity: sha512-7rCnleh0z2CkXhH67J8K1Ytz0b2Y+yxTPL+/KOJoa20hfnVQ/3/T6W/KflYI4bRHRagNeXeU2bkNGI3v1oS/lw==}
 
@@ -2935,6 +2974,10 @@ packages:
 
   jwa@2.0.1:
     resolution: {integrity: sha512-hRF04fqJIP8Abbkq5NKGN0Bbr3JxlQ+qhZufXVr0DvujKy93ZCbXZMHDL4EOtodSbCWxOqR8MS1tXA5hwqCXDg==}
+
+  jwks-rsa@3.2.0:
+    resolution: {integrity: sha512-PwchfHcQK/5PSydeKCs1ylNym0w/SSv8a62DgHJ//7x2ZclCoinlsjAfDxAAbpoTPybOum/Jgy+vkvMmKz89Ww==}
+    engines: {node: '>=14'}
 
   jws@3.2.2:
     resolution: {integrity: sha512-YHlZCB6lMTllWDtSPHz/ZXTsi8S00usEV6v1tjq8tOUZzw7DpSDWVXjXDre6ed1w/pd495ODpHZYSdkRTsa0HA==}
@@ -3040,12 +3083,18 @@ packages:
     resolution: {integrity: sha512-utWOt/GHzuUxnLKxB6dk81RoOeoNeHgbrXiuGk4yyF5qlRz+iIVWu56E2fqGHFrXz0QNUhLB/8nKqvRH66JKGQ==}
     engines: {node: '>=10'}
 
+  limiter@1.1.5:
+    resolution: {integrity: sha512-FWWMIEOxz3GwUI4Ts/IvgVy6LPvoMPgjMdQ185nN6psJyBJ4yOpzqm695/h5umdLJg2vW3GR5iG11MAkR2AzJA==}
+
   locate-character@3.0.0:
     resolution: {integrity: sha512-SW13ws7BjaeJ6p7Q6CO2nchbYEc3X3J6WrmTTDto7yMPqVSZTUyY5Tjbid+Ab8gLnATtygYtiDIJGQRRn2ZOiA==}
 
   locate-path@6.0.0:
     resolution: {integrity: sha512-iPZK6eYjbxRu3uB4/WZ3EsEIMJFMqAoopl3R+zuq0UjcAm/MO6KCweDgPfP3elTztoKP3KtnVHxTn2NHBSDVUw==}
     engines: {node: '>=10'}
+
+  lodash.clonedeep@4.5.0:
+    resolution: {integrity: sha512-H5ZhCF25riFd9uB5UCkVKo61m3S/xZk1x4wA6yp/L3RFP6Z/eHH1ymQcGLo7J3GMPfm0V/7m1tryHuGVxpqEBQ==}
 
   lodash.includes@4.3.0:
     resolution: {integrity: sha512-W3Bx6mdkRTGtlJISOvVD/lbqjTlPPUDTMnlXZFnVwi9NKJ6tiAk6LVdlhZMm17VZisqhKcgzpO5Wz91PCt5b0w==}
@@ -3086,6 +3135,9 @@ packages:
   lru-cache@6.0.0:
     resolution: {integrity: sha512-Jo6dJ04CmSjuznwJSS3pUeWmd/H0ffTlkXXgwZi+eq1UCmqQwCh+eLsYOYCwY991i2Fah4h1BEMCx4qThGbsiA==}
     engines: {node: '>=10'}
+
+  lru-memoizer@2.3.0:
+    resolution: {integrity: sha512-GXn7gyHAMhO13WSKrIiNfztwxodVsP8IoZ3XfrJV4yH2x0/OeTO/FIaAHTY5YekdGgW94njfuKmyyt1E0mR6Ug==}
 
   lz-string@1.5.0:
     resolution: {integrity: sha512-h5bgJWpxJNswbU7qCrV0tIKQCaS3blPDrqKWx+QxzuzL1zGUzij9XCWLrSLsJPu5t+eWA/ycetzYAO5IOMcWAQ==}
@@ -5564,9 +5616,34 @@ snapshots:
 
   '@types/aria-query@5.0.4': {}
 
+  '@types/body-parser@1.19.6':
+    dependencies:
+      '@types/connect': 3.4.38
+      '@types/node': 22.15.18
+
+  '@types/connect@3.4.38':
+    dependencies:
+      '@types/node': 22.15.18
+
   '@types/cookie@0.6.0': {}
 
   '@types/estree@1.0.7': {}
+
+  '@types/express-serve-static-core@4.19.7':
+    dependencies:
+      '@types/node': 22.15.18
+      '@types/qs': 6.14.0
+      '@types/range-parser': 1.2.7
+      '@types/send': 1.2.1
+
+  '@types/express@4.17.25':
+    dependencies:
+      '@types/body-parser': 1.19.6
+      '@types/express-serve-static-core': 4.19.7
+      '@types/qs': 6.14.0
+      '@types/serve-static': 1.15.10
+
+  '@types/http-errors@2.0.5': {}
 
   '@types/json-schema@7.0.15': {}
 
@@ -5577,6 +5654,8 @@ snapshots:
 
   '@types/mdx@2.0.13': {}
 
+  '@types/mime@1.3.5': {}
+
   '@types/ms@2.1.0': {}
 
   '@types/node@22.15.18':
@@ -5585,9 +5664,28 @@ snapshots:
 
   '@types/pug@2.0.10': {}
 
+  '@types/qs@6.14.0': {}
+
+  '@types/range-parser@1.2.7': {}
+
   '@types/react@19.1.4':
     dependencies:
       csstype: 3.1.3
+
+  '@types/send@0.17.6':
+    dependencies:
+      '@types/mime': 1.3.5
+      '@types/node': 22.15.18
+
+  '@types/send@1.2.1':
+    dependencies:
+      '@types/node': 22.15.18
+
+  '@types/serve-static@1.15.10':
+    dependencies:
+      '@types/http-errors': 2.0.5
+      '@types/node': 22.15.18
+      '@types/send': 0.17.6
 
   '@types/uuid@9.0.8': {}
 
@@ -7019,6 +7117,8 @@ snapshots:
       '@sideway/pinpoint': 2.0.0
     optional: true
 
+  jose@4.15.9: {}
+
   js-base64@3.7.7: {}
 
   js-sha256@0.11.0: {}
@@ -7106,6 +7206,17 @@ snapshots:
       buffer-equal-constant-time: 1.0.1
       ecdsa-sig-formatter: 1.0.11
       safe-buffer: 5.2.1
+
+  jwks-rsa@3.2.0:
+    dependencies:
+      '@types/express': 4.17.25
+      '@types/jsonwebtoken': 9.0.9
+      debug: 4.4.1
+      jose: 4.15.9
+      limiter: 1.1.5
+      lru-memoizer: 2.3.0
+    transitivePeerDependencies:
+      - supports-color
 
   jws@3.2.2:
     dependencies:
@@ -7197,11 +7308,15 @@ snapshots:
 
   lilconfig@2.1.0: {}
 
+  limiter@1.1.5: {}
+
   locate-character@3.0.0: {}
 
   locate-path@6.0.0:
     dependencies:
       p-locate: 5.0.0
+
+  lodash.clonedeep@4.5.0: {}
 
   lodash.includes@4.3.0: {}
 
@@ -7232,6 +7347,11 @@ snapshots:
   lru-cache@6.0.0:
     dependencies:
       yallist: 4.0.0
+
+  lru-memoizer@2.3.0:
+    dependencies:
+      lodash.clonedeep: 4.5.0
+      lru-cache: 6.0.0
 
   lz-string@1.5.0: {}
 

src/lib/server/google.ts
@@ -10,6 +10,7 @@ import jwt from "jsonwebtoken"
 import { z } from "zod"
 import * as auth from "$lib/server/auth"
 import { v4 as uuid } from "uuid"
+import { JwksClient } from "jwks-rsa"
 
 const idTokenSchema = z.object({
   email: z.string(),
@@ -55,23 +56,35 @@ export async function redirectToGoogle(state: Omit<table.AuthState, "id">) {
   return redirect(302, authUrl)
 }
 
-const keySchema = z.object({
-  e: z.string(),
-  kty: z.string(),
-  n: z.string(),
-  use: z.string(),
-  alg: z.string(),
-  kid: z.string(),
+const jwsClient = new JwksClient({
+  jwksUri: "https://www.googleapis.com/oauth2/v3/certs",
+  requestHeaders: {},
+  timeout: 30000,
 })
-const keysSchema = z.object({ keys: z.array(keySchema) })
 
-async function getGooglePublicKey(): Promise<jwt.PublicKey> {
-  // TODO: Cache the keys and only refetch if decoding fails.
-  const response = await fetch("https://www.googleapis.com/oauth2/v3/certs")
-  const data = keysSchema.parse(await response.json())
-  // TODO: Why do we use the first key from Google? Is it always?
-  const key = { key: data.keys[0], format: "jwk" as const }
-  return key
+function verifyToken(token: string) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(
+      token,
+      (header, callback) => {
+        jwsClient.getSigningKey(header.kid, (error, key) => {
+          if (error) {
+            reject(error)
+          } else {
+            const signingKey = key?.getPublicKey()
+            callback(null, signingKey)
+          }
+        })
+      },
+      (error, decoded) => {
+        if (error) {
+          reject(error)
+        } else {
+          resolve(decoded)
+        }
+      },
+    )
+  })
 }
 
 export async function handleGoogleCallback(event: RequestEvent) {
@@ -108,7 +121,8 @@ export async function handleGoogleCallback(event: RequestEvent) {
 
   if (!tokens.id_token) throw new Error("No ID token found.")
 
-  const decoded = jwt.verify(tokens.id_token, await getGooglePublicKey())
+  console.log("tokens", tokens)
+  const decoded = await verifyToken(tokens.id_token)
   const idToken = idTokenSchema.parse(decoded)
   console.log("decoded", decoded)
   console.log("idToken", idToken)