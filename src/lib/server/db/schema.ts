nicholaschiang/gather

commit 303e43d9edee239e4c2c56a442151838d1a3e1bb

feat: add `created_at` and `updated_at` columns
Ref: https://orm.drizzle.team/docs/guides/timestamp-default-value#sqlite

src/lib/server/db/schema.ts
@@ -1,3 +1,4 @@
+import { sql } from "drizzle-orm"
 import { sqliteTable, text, integer, primaryKey } from "drizzle-orm/sqlite-core"
 
 export const user = sqliteTable("user", {
@@ -11,6 +12,12 @@ export const user = sqliteTable("user", {
     .notNull()
     .default(false),
   googleRefreshToken: text("google_refresh_token").notNull().unique(),
+  createdAt: text("created_at")
+    .notNull()
+    .default(sql`(current_timestamp)`),
+  updatedAt: text("updated_at")
+    .notNull()
+    .default(sql`(current_timestamp)`),
 })
 export type User = typeof user.$inferSelect
 
@@ -20,6 +27,12 @@ export const session = sqliteTable("session", {
     .notNull()
     .references(() => user.id, { onDelete: "cascade", onUpdate: "cascade" }),
   expiresAt: integer("expires_at", { mode: "timestamp" }).notNull(),
+  createdAt: text("created_at")
+    .notNull()
+    .default(sql`(current_timestamp)`),
+  updatedAt: text("updated_at")
+    .notNull()
+    .default(sql`(current_timestamp)`),
 })
 export type Session = typeof session.$inferSelect
 
@@ -30,6 +43,12 @@ export const authState = sqliteTable("auth_state", {
     () => gathering.id,
     { onDelete: "set null", onUpdate: "set null" },
   ),
+  createdAt: text("created_at")
+    .notNull()
+    .default(sql`(current_timestamp)`),
+  updatedAt: text("updated_at")
+    .notNull()
+    .default(sql`(current_timestamp)`),
 })
 export type AuthState = typeof authState.$inferSelect
 
@@ -42,6 +61,12 @@ export const gathering = sqliteTable("gathering", {
   creatorId: text("creator_id")
     .notNull()
     .references(() => user.id, { onDelete: "cascade", onUpdate: "cascade" }),
+  createdAt: text("created_at")
+    .notNull()
+    .default(sql`(current_timestamp)`),
+  updatedAt: text("updated_at")
+    .notNull()
+    .default(sql`(current_timestamp)`),
 })
 export type Gathering = typeof gathering.$inferSelect
 
@@ -57,6 +82,12 @@ export const relUserGathering = sqliteTable(
         onDelete: "cascade",
         onUpdate: "cascade",
       }),
+    createdAt: text("created_at")
+      .notNull()
+      .default(sql`(current_timestamp)`),
+    updatedAt: text("updated_at")
+      .notNull()
+      .default(sql`(current_timestamp)`),
   },
   (table) => [primaryKey({ columns: [table.userId, table.gatheringId] })],
 )

commit 004b6576c540e6e5d1a84894674ed3b8ea0814b5

fix: only require fields needed at insert
Fixes: 303e43d ("feat: add `created_at` and `updated_at` columns")

src/lib/server/db/schema.ts
@@ -20,6 +20,7 @@ export const user = sqliteTable("user", {
     .default(sql`(unixepoch())`),
 })
 export type User = typeof user.$inferSelect
+export type UserInsert = typeof user.$inferInsert
 
 export const relUserFollow = sqliteTable("rel_user_follow", {
   followerId: text("follower_id")
@@ -50,6 +51,7 @@ export const session = sqliteTable("session", {
     .default(sql`(unixepoch())`),
 })
 export type Session = typeof session.$inferSelect
+export type SessionInsert = typeof session.$inferInsert
 
 export const authState = sqliteTable("auth_state", {
   id: text("id").primaryKey(),
@@ -66,6 +68,7 @@ export const authState = sqliteTable("auth_state", {
     .default(sql`(unixepoch())`),
 })
 export type AuthState = typeof authState.$inferSelect
+export type AuthStateInsert = typeof authState.$inferInsert
 
 export const gathering = sqliteTable("gathering", {
   id: text("id").primaryKey(),
@@ -84,6 +87,7 @@ export const gathering = sqliteTable("gathering", {
     .default(sql`(unixepoch())`),
 })
 export type Gathering = typeof gathering.$inferSelect
+export type GatheringInsert = typeof gathering.$inferInsert
 
 export const relUserGathering = sqliteTable(
   "rel_user_gathering",

src/lib/server/google.ts
@@ -28,7 +28,7 @@ const scopes = [
   "https://www.googleapis.com/auth/calendar.readonly",
 ]
 
-export async function redirectToGoogle(state: Omit<table.AuthState, "id">) {
+export async function redirectToGoogle(state: Omit<table.AuthStateInsert, "id">) {
   // Generate a secure random state value.
   // TODO Should I be storing this in a cookie as well?
   const stateId = randomBytes(32).toString("base64url")
@@ -159,6 +159,8 @@ export async function handleGoogleCallback(event: RequestEvent) {
       email: idToken.email,
       emailVerified: idToken.email_verified,
       googleRefreshToken: tokens.refresh_token,
+      createdAt: new Date(),
+      updatedAt: new Date(),
     }
     await db.insert(table.user).values(user)
 