e24df7efda0dc40c3be169a0d316cf3ef8f93b55
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2025-11-03 01:31:38 -0700

    style: automatically format code with `prettier`
    ```
    just format
    ```
    
    ...was run to generate the contents of this patch.

--- src/routes/new/gathering/[friendId]/constants.ts
+++ src/routes/new/gathering/[friendId]/constants.ts
@@ -1,5 +1,5 @@
 // The height of each hour row in the calendar grid.
-export const HOUR_HEIGHT = 48;
+export const HOUR_HEIGHT = 48
 
 // The height of the headers in the calendar grid.
-export const HEADER_HEIGHT = 32;
+export const HEADER_HEIGHT = 32
