cf81d76f1d02cdfeba584a92f63365319aa16b4c
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2025-11-16 15:21:53 -0700

    feat: add "go back home" button to error page
    Ref: https://svelte.dev/docs/kit/errors

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button"
+  import { page } from "$app/state"
+</script>
+
+<main class="flex h-screen w-screen flex-col items-center justify-center p-2">
+  <h1>{page.status}</h1>
+  <h2>{page.error?.message ?? "Error"}</h2>
+  <Button href="/" size="sm" class="mt-8">Go back home</Button>
+</main>
