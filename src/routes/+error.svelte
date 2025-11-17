565a2a59558fe4c34f3906fc722fd4de1cd0cb69
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2025-11-16 17:22:21 -0700

    feat: make the error "go back" button bigger
    Fixes: 73df203 ("feat: add "go back home" button to error page")

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -6,5 +6,5 @@
 <main class="flex h-screen w-screen flex-col items-center justify-center p-2">
   <h1>{page.status}</h1>
   <h2>{page.error?.message ?? "Error"}</h2>
-  <Button href="/" size="sm" class="mt-8">Go back home</Button>
+  <Button href="/" class="mt-8">Go back home</Button>
 </main>

nicholaschiang/gather

commit 73df203fc8f4a96df19ef2a9a5ddbe4c22c4acfb

feat: add "go back home" button to error page
Ref: https://svelte.dev/docs/kit/errors

src/routes/+error.svelte
@@ -1,0 +1,10 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button"
+  import { page } from "$app/state"
+</script>
+
+<main class="flex h-screen w-screen flex-col items-center justify-center p-2">
+  <h1>{page.status}</h1>
+  <h2>{page.error?.message ?? "Error"}</h2>
+  <Button href="/" size="sm" class="mt-8">Go back home</Button>
+</main>

commit 565a2a59558fe4c34f3906fc722fd4de1cd0cb69

feat: make the error "go back" button bigger
Fixes: 73df203 ("feat: add "go back home" button to error page")

src/routes/+error.svelte
@@ -6,5 +6,5 @@
 <main class="flex h-screen w-screen flex-col items-center justify-center p-2">
   <h1>{page.status}</h1>
   <h2>{page.error?.message ?? "Error"}</h2>
-  <Button href="/" size="sm" class="mt-8">Go back home</Button>
+  <Button href="/" class="mt-8">Go back home</Button>
 </main>