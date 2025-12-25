aa434aa393bcdca9639230ca60d1592b5641f847
Nicholas Chiang <nicholas.chiang@numbersstation.ai>
2025-12-25 01:24:54 -0800

    fix: add more helpful error page for new gatherings

---
+++ src/routes/new/gathering/[friendId]/+error.svelte
@@ -0,0 +1,7 @@
+<main class="flex h-screen w-screen flex-col items-center justify-center p-2">
+  <p>
+    We could not fetch calendar information for you or your friend. You can
+    still schedule a gathering with them, and we'll let them know to re-sync
+    their availability.
+  </p>
+</main>
